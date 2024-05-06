e240901e91a4e6b144126e2856386545a4617f83
Aditya <adityamtambe@gmail.com>
2024-05-06 11:50:35 -0400

    new file

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
+
+PINECONE_API_KEY = "ab697d60-75e3-4cfe-88d9-83f88f8a025b"
+
+def load_pdf(data):
+    loader = DirectoryLoader(data, 
+                    glob="*.pdf",
+                    loader_cls=PyPDFLoader)
+    documents = loader.load()
+
+    return documents
+
+extracted_data = load_pdf("data/")
