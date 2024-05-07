b75cb0d88ae68e05f31bc3b0cef588bf03520dd3
Aditya <adityamtambe@gmail.com>
2024-05-06 20:09:55 -0400

    new file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from langchain import PromptTemplate
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain_core.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Pinecone
 import pinecone
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
+from langchain_community.output_parsers.rail_parser import GuardrailsOutputParser
+##from langchain.chains import RetrievalQA
+
 
 PINECONE_API_KEY = "ab697d60-75e3-4cfe-88d9-83f88f8a025b"
 
@@ -19,3 +20,11 @@ def load_pdf(data):
     return documents
 
 extracted_data = load_pdf("data/")
+
+def text_split(extracted_data):
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
+
